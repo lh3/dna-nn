a80d1e7c6517b8b645879bfab46780da2063a895
Heng Li <lh3@me.com>
2018-12-18 09:19:30 -0500

    switch the label for ALU and ALPHA

--- parse-rm.js
+++ parse-rm.js
@@ -27,8 +27,6 @@ for (var i = 0; i < motif_alt.length; ++i)
 
 for (var i = 0; i < motif.length; ++i) motif_hash[motif[i]] = i;
 
-//for (var i = 0; i < motif.length; ++i) print(motif[i]);
-
 // print usage
 if (arguments.length == 0) {
 	print("Usage: k8 parse-rm.js <rmsk.out>");
@@ -43,21 +41,21 @@ while (file.readline(buf) >= 0) {
 	var m, m4, line = buf.toString();
 	if ((m = /^\s*\d+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+(\d+)\s+(\d+)\s+\S+\s+[+C]\s+(\S+)\s+(\S+)/.exec(line)) != null) {
 		var type = null;
-		if (m[5] == "SINE/Alu") type = "1";
-		else if (m[4] == "ALR/Alpha") type = "3";
+		if (m[5] == "SINE/Alu") type = "3";
+		else if (m[4] == "ALR/Alpha") type = "2";
 		else if (m[4] == "BSR/Beta" || m[4] == "LSAU") type = "4";
-		else if (m[4] == "HSATII") type = "2";
+		else if (m[4] == "HSATII") type = "1";
 		else if (m[5] == "LINE/L1") type = "5";
 		else if ((m[5] == "Simple_repeat" || m[5] == "Satellite") && ((m4 = /^\(([ACGT]+)\)n/.exec(m[4])) != null)) {
 			if (motif_hash[m4[1]] != null) {
-				type = "2";
+				type = "1";
 			} else if (m4[1].length % motif0.length == 0) {
 				var c = 0;
 				for (var j = 0; j < m4[1].length; j += motif0.length)
 					if (motif_hash[m4[1].substr(j, j + motif0.length)] != null)
 						++c;
 				if (c * motif0.length == m4[1].length)
-					type = "2";
+					type = "1";
 			}
 		}
 		if (type != null) print(m[1], parseInt(m[2]) - 1, m[3], type, m[4], m[5]);
