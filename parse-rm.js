40dae53fddfcb806197e024491a86bec161c218b
Heng Li <lh3@me.com>
2019-01-20 13:25:51 -0500

    identify another type of hsat2,3

--- parse-rm.js
+++ parse-rm.js
@@ -3,6 +3,7 @@
 var motif0 = "GGAAT";
 var comp_tbl = { 'A':'T', 'T':'A', 'C':'G', 'G':'C' };
 var motif = [motif0], motif_alt = [], motif_hash = {};
+var motif_mut_hash = {};
 
 // reverse complement
 for (var i = 0; i < motif.length; ++i) {
@@ -26,6 +27,20 @@ for (var i = 0; i < motif_alt.length; ++i)
 	motif.push(motif_alt[i]);
 
 for (var i = 0; i < motif.length; ++i) motif_hash[motif[i]] = i;
+
+// mutate
+var bases = [ 'A', 'C', 'G', 'T' ];
+for (var x in motif_hash) {
+	var y = x;
+	for (var i = 0; i < x.length; ++i) {
+		for (var j = 0; j < bases.length; ++j) {
+			var a = x.split("");
+			if (a[i] == bases[j]) continue;
+			a[i] = bases[j];
+			motif_mut_hash[a.join("")] = 1;
+		}
+	}
+}
 
 // print usage
 if (arguments.length == 0) {
@@ -57,11 +72,15 @@ while (file.readline(buf) >= 0) {
 			if (motif_hash[m4[1]] != null) {
 				type = "1";
 			} else if (m4[1].length % motif0.length == 0) {
-				var c = 0;
-				for (var j = 0; j < m4[1].length; j += motif0.length)
-					if (motif_hash[m4[1].substr(j, j + motif0.length)] != null)
+				var c = 0, c_mut = 0;
+				for (var j = 0; j < m4[1].length; j += motif0.length) {
+					var s = m4[1].substr(j, j + motif0.length);
+					if (motif_hash[s] != null)
 						++c;
-				if (c * motif0.length == m4[1].length)
+					else if (motif_mut_hash[s] != null)
+						++c_mut;
+				}
+				if (c > 0 && (c + c_mut) * motif0.length == m4[1].length)
 					type = "1";
 			}
 		}
